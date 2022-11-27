835572c92b2813212a8bae9faa917f55b184c5ff
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-27 14:22:17 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,62 @@
+//Destruction assignment
+//use array and collection of objects[]
+// We wii use dog name and breed as variables
+
+// code solution
+
+//constants
+const farmAnimals = 'cow horse sheep pig chicken';
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const muppet = {
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit'
+};
+const nestedMuppet = {
+  nestedName: 'Kermit',
+  nestedColor: 'green',
+  album: {
+    theMuppetMovie: {
+      song1: 'Rainbow Connection',
+      song2: 'Moving Right Along',
+      song3: 'Never Before, Never Again',
+      song4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
+};
+
+// Strings
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ")
+
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ")
+
+
+const [blackAndWhite, ,black, pink] = farmAnimals.split(" ")
+
+// Arrays
+
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
+
+const [r, o, y, g, b, , v] = colors
+
+
+const [, , , , , indg, ,] = colors
+
+// Objects
+
+s
+const {muppetName, color, song, job, partner} = muppet
+
+
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+const {nestedJob, nestedPartner} = nestedMuppet
